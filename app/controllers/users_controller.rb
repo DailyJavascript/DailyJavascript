cc5a28262713ba385f50e463845d82029c53af39
unknown <salr3467@gmail.com>
2018-10-23 18:07:24 -0400

    testing cors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,15 @@ class UsersController < ApplicationController
 		render 'index'
 	end
 
+	def preflight
+		render plain: "proceed"
+	end
+
 	def create
-		puts " "
-		puts " "
-		puts params[:email]
-		puts " "
-		puts request.headers
-		puts " "
-		puts request.headers.inspect
 		t = DateTime.now
 		u = User.new(email: params[:email], email_verified: false, email_verification_code: nil, unsubscribe_code: nil, date_joined: t, membership_level: params[:membershipLevel], date_current_membership_level: t, membership_level_history: nil)
 		if u.save!
-			render plain: "good good"
+			render plain: "good"
 		else 
 			render plain: "bad"
 		end
