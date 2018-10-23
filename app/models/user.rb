0b3dffdde4a933960ddadb3cc5105a72236820c2
unknown <salr3467@gmail.com>
2018-10-23 19:56:14 -0400

    creating email verification code

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ require 'securerandom'
 def self.createEmailVerificationCode
 	evc = SecureRandom.hex(18)
 	u = User.find_by(email_verification_code: evc)
-	while !u.empty?
+	while !u.present?
 		evc = SecureRandom.hex(18)
 		u = User.find_by(email_verification_code: evc)
 	end
