dfaa69b7844badefb4bf1c6e7d7fd0faadfb718c
unknown <salr3467@gmail.com>
2018-11-10 19:48:06 -0500

    setup actionmailer layouts and views and triggered send email on successful signup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
 	default from: 'dailyjavascript@sendgrid.net'
 
-	def welcome_email(em)
+	def welcome_email
 		email = em
 		mail(to: em, subject: 'Welcome to Daily Javascript')
 	end
