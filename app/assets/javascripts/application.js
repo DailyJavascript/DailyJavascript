ea03c7c3296ed2cc7c488e0d80c84d9e63304447
unknown <salr3467@gmail.com>
2018-11-13 20:11:39 -0500

    adding submission of challenge text

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,22 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+
+function submitChallenge(code) {
+  var challenge = document.getElementById("challengeText").value;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var response = this.responseText + "";
+        if (response == "good") {
+          alert("Successful");
+        } else if (response == "bad") {
+          alert("Failed");
+        } // end if...else response
+    } // end if (this.readyState == 4 && this.status == 200)
+  } // end xhttp.onreadystatechange = function()
+  xhttp.open("POST","https://dailyjavascript.herokuapp.com/challenges",true);
+  xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  xhttp.send("challenge="+challenge+"&code="+code);
+} // end function
--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -4,5 +4,18 @@ class ChallengesController < ApplicationController
 	def new
 	end
 
+	def create
+		puts params[:challenge]
+		puts params[:code]
+		c = Challenge.new(challenge: params[:challenge])
+		if c.save
+			if params[:code].to_i == 1
+				Challenge.mail_next_challenge
+			end
+			render plain: "good"
+		else
+			render plain: "bad"
+		end
+	end
 
 end
