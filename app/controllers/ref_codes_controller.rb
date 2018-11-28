888df7a9aebb68316a820c40c619cd9ad0dfe7a2
unknown <salr3467@gmail.com>
2018-11-28 17:14:42 -0500

    added refcodes controller and view to generate ref codes

---
+++ app/controllers/ref_codes_controller.rb
@@ -0,0 +1,9 @@
+class RefCodesController < ApplicationController
+
+	def index
+	end
+
+	def create
+	end
+
+end
--- db/migrate/20181018042229_create_ref_codes.rb
+++ db/migrate/20181018042229_create_ref_codes.rb
@@ -2,6 +2,7 @@ class CreateRefCodes < ActiveRecord::Migration[5.1]
   def change
     create_table :ref_codes do |t|
       t.string :refcode
+      t.text :refcode_url
       t.text :destination_url
       t.string :destination_name
 
