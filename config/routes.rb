7d6b7db698b6414e336cdd64749528bedd2a19d3
unknown <salr3467@gmail.com>
2018-12-04 15:03:16 -0500

    debugging

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :challenges
-  resources :payments
-  resources :ref_codes
-
+  
   get '/users/preflight', to: "users#preflight"
   get '/private/subs', to: "users#show_subscriptions"
   get '/private/challenges', to: "challenges#new"
   get '/users/unsubscribe', to: "users#unsubscribe"
   get '/ref_codes/all', to:"ref_codes#show_all"
+
+  resources :users
+  resources :challenges
+  resources :payments
+  resources :ref_codes
 
 end
