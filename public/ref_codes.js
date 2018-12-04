6310fe2ac5035eae11225e95b0c5bf7318e79103
unknown <salr3467@gmail.com>
2018-12-03 23:39:23 -0500

    t

--- public/ref_codes.js
+++ public/ref_codes.js
@@ -1,20 +1,19 @@
 class InputFields extends React.Component {
-
 	render() {
 		return (
 			<form>
 				<label>
 					Destination Name
 				</label>
-				<input type="text" name="destinationName" id="destinationName" />
+				<input type="text" name="destinationName" id="destinationName" onChange=props.handleInput value=props.values.destinationName />
 				<label>
 					Destination URL
 				</label>
-				<input type="text" name="destinationURL" id="DestinationURL" />
+				<input type="text" name="destinationURL" id="DestinationURL" onChange=props.handleInput value=props.values.destinationURL />
 				<label>
 					Email
 				</label>
-				<input type="text" name="email" id="email" />
+				<input type="text" name="email" id="email" onChange=props.handleInput value=props.values.email />
 				<label>
 					Email Content
 				</label>
@@ -26,4 +25,32 @@ class InputFields extends React.Component {
 } // end class InputFields
 
 
-ReactDOM.render(<InputFields />, document.getElementById("root"));
+class RefCodes extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { 
+			email: '',
+			destinationName: '',
+			destinationURL: ''
+		};
+	}
+
+	handleInput = e => {
+		const {id,value} = e.target;
+		this.setState({
+			id:value
+		});
+	}
+
+
+	render() {
+		return <InputFields handleInput=this.handleInput values=this.state />;
+	}
+
+
+
+} // end class RefCodes
+
+
+
+ReactDOM.render(<RefCodes />, document.getElementById("root"));
