706020f53e3beaf25b7edf111216fcdeb3e98efe
unknown <salr3467@gmail.com>
2018-12-04 20:52:41 -0500

    ref_codes front-end react working

--- app/controllers/ref_codes_controller.rb
+++ app/controllers/ref_codes_controller.rb
@@ -8,7 +8,8 @@ layout "ref_codes"
 	end
 
 	def show_all
-		render json: [{recipientName:"bootcamp",recipientURL:"http://bootcamp",recipientEmail:"watid2005"},{recipientName:"college",recipientURL:"http://college",recipientEmail:"farmingdale"}]
+		#render json: [{recipientName:"bootcamp",recipientURL:"http://bootcamp",recipientEmail:"watid2005"},{recipientName:"college",recipientURL:"http://college",recipientEmail:"farmingdale"}]
+		render json: []
 	end
 
 end
