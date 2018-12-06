531762d33aeb33d1511ff2986430bd06060afdd3
unknown <salr3467@gmail.com>
2018-12-05 21:48:12 -0500

    a

--- public/ref_codes.js
+++ public/ref_codes.js
@@ -82,10 +82,7 @@ class ListOfRefCodes extends React.Component {
 	}
 
 	render() {
-		const listItems = this.state.list.map((listItem, index) => 
-			{if (index == this.state.list.length-1) return <RefListing key={listItem.recipientURL} listItem={listItem}/>; else return 
-			(<div><RefListing key={listItem.recipientURL} listItem={listItem}/><br /><span>****</span></div>);});
-		
+		const listItems = this.state.list.map((listItem) => <RefListing key={listItem.recipientURL} listItem={listItem}/>);
 		if (!listItems || listItems.length == 0)
 			return (<div>No List Items Yet</div>);
 		else 
