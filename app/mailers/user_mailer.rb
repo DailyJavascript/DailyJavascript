086e0eb4acbca1b5166aefe46f0fe96f3ba1404c
unknown <salr3467@gmail.com>
2018-12-06 06:36:40 -0500

    added mailer to be sent when refcode is created

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,4 +12,11 @@ class UserMailer < ApplicationMailer
 		@next_challenge = Challenge.find(challenge_id)
 		mail(to: user_email_address, subject: "Today's Daily Javascript email")
 	end
+
+	def invite_email(recipient_email, status, email_content, rc)
+		@status = status
+		@email_content = email_content
+		@link = "https://www.dailyjavascript.io?"+rc
+		mail(to: recipient_email, subject: "Improve your programming skills and get prepped for your coding interview - become a member of Daily Javascript")
+	end
 end
