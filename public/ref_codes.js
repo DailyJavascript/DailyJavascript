bacea0135467ec522588d1410ace8c1ded44ef9a
unknown <salr3467@gmail.com>
2018-12-06 10:29:19 -0500

    test css with reaect

--- public/ref_codes.js
+++ public/ref_codes.js
@@ -68,7 +68,7 @@ class InputFields extends React.Component {
 
 class ListOfRefCodes extends React.Component {
 	render() { 
-		const listItems = this.props.list.map((listItem, index) => {var a = <RefListing key={listItem.recipientURL} listItem={listItem}/>; var b = (<div><br /><span>****</span></div>); if (index == this.props.list.length-1) return (<div className="listing">{a}</div>); else return (<div className="listing">{a}{b}</div>);});
+		const listItems = this.props.list.map((listItem, index) => {var a = <RefListing key={listItem.recipientURL} listItem={listItem}/>; var b = (<div><br /><span>****</span></div>); if (index == this.props.list.length-1) return (<div className="listing-pre">{a}</div>); else return (<div className="listing-pre">{a}{b}</div>);});
 		if (!listItems || listItems.length == 0)
 			return (<div>No List Items Yet</div>);
 		else 
