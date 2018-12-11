6d4e811d86fda0b465ed2aafba17d9e9c749127b
unknown <salr3467@gmail.com>
2018-12-10 20:11:40 -0500

    debugging adding new challenges

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -8,12 +8,11 @@ class ChallengesController < ApplicationController
 		puts params[:challenge]
 		puts params[:code]
 		c = Challenge.new(challenge: params[:challenge])
+		if (Challenge.count > 0)
+			current_id = Challenge.order(:id).last.id.to_i + 1
+			c.id = current_id
+		end		
 		if c.save
-			if (Challenge.count > 0)
-				current_id = Challenge.order(:id).last.id.to_i + 1
-				c.id = current_id
-				c.save
-			end
 			if params[:code].to_i == 1
 				Challenge.mail_next_challenge
 			end
