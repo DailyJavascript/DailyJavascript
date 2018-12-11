943a00572d4d428e68dad147f268f6a6e33ad7fc
afr114 <afr114@gmail.com>
2018-12-10 23:30:04 -0800

    add mailer previews and make sure to not include schema and gemfile.lock in git commits

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def welcome_email
+    UserMailer.with(user: User.first).welcome_email(User.first.email, User.first.id)
+  end
 
+  def next_challenge_email
+    UserMailer.with(user: User.first).next_challenge_email(1, User.first.email, User.first.id)
+  end
 end
