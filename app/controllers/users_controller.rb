dfd694738c1734147457147a810eda3ec13fbb23
unknown <salr3467@gmail.com>
2018-12-16 10:34:06 -0500

    debugging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,11 +63,15 @@ class UsersController < ApplicationController
 			if user_challenges.present?
 				user_challenges.delete_all
 			end
-			Unsubscribed.create(email: user.email, email_verified: user.email_verified, email_verification_code: user.email_verification_code, unsubscribe_code: user.unsubscribe_code, date_joined: user.date_joined, membership_level: user.membership_level, date_current_membership_level: user.date_current_membership_level, membership_level_history: user.membership_level_history, active: user.active) 
+			atts = user.attributes.except(:id)
+			puts atts
+			Unsubscribed.create(atts) 
 			user.delete 
 			@unsubscribe_result = 1
 		end
 	end
+
+#email: user.email, email_verified: user.email_verified, email_verification_code: user.email_verification_code, unsubscribe_code: user.unsubscribe_code, date_joined: user.date_joined, membership_level: user.membership_level, date_current_membership_level: user.date_current_membership_level, membership_level_history: user.membership_level_history, active: user.active
 
 	def show_subscriptions
 		@users = User.order(:id)
