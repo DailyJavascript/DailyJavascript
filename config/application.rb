29bc06e3f20d05966da054041a71b120b9640f21
afr114 <afr114@gmail.com>
2018-12-18 22:01:43 -0800

    add dailyjavascriptblog to CORs list

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Dailyjavascript
 
     config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
     	allow do
-    		origins 'https://www.dailyjavascript.io', 'http://www.dailyjavascript.io', 'http://dailyjavascript.io'
+    		origins 'https://www.dailyjavascript.io', 'http://www.dailyjavascript.io', 'http://dailyjavascript.io', 'https://dailyjavascriptblog.netlify.com/'
     		resource '*', headers: :any, methods: [:get, :post, :options, :put, :patch, :delete]
     	end
     end
