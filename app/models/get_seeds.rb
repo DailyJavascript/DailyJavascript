df3831902ac1517e84333ddab120c57dc3013f6f
unknown <salr3467@gmail.com>
2019-01-01 11:52:01 -0500

    updated seeds

--- app/models/get_seeds.rb
+++ app/models/get_seeds.rb
@@ -3,7 +3,7 @@ class GetSeeds < ApplicationRecord
 
 	def self.get_refcodes
 		RefCode.order(:id).each do |x|
-			puts "RefCode.create(RefCode.create(ref_code:"+x.ref_code.inspect+", ref_code_url:"+x.ref_code_url.inspect+", recipient_name:"+x.recipient_name.inspect+", recipient_url:"+x.recipient_url.inspect+")"
+			puts "RefCode.create(ref_code:"+x.ref_code.inspect+", ref_code_url:"+x.ref_code_url.inspect+", recipient_name:"+x.recipient_name.inspect+", recipient_url:"+x.recipient_url.inspect+")"
 		end
 	end
 
