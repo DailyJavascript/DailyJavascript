2f7dd18da8e209b86114515fd748e725ab4a2ab0
unknown <salr3467@gmail.com>
2019-01-08 22:48:01 -0500

    updated create seeds functions

--- app/models/get_seeds.rb
+++ app/models/get_seeds.rb
@@ -3,13 +3,13 @@ class GetSeeds < ApplicationRecord
 
 	def self.get_refcodes
 		RefCode.order(:id).each do |x|
-			puts "RefCode.create(ref_code:"+x.ref_code.inspect+", ref_code_url:"+x.ref_code_url.inspect+", recipient_name:"+x.recipient_name.inspect+", recipient_url:"+x.recipient_url.inspect+")"
+			puts "RefCode.create(id: "+x.id.inspect+", ref_code:"+x.ref_code.inspect+", ref_code_url:"+x.ref_code_url.inspect+", recipient_name:"+x.recipient_name.inspect+", recipient_url:"+x.recipient_url.inspect+")"
 		end
 	end
 
 	def self.get_challenges
 		Challenge.order(:id).each do |x|
-			puts "Challenge.create(challenge:"+x.challenge.inspect+", challenge_no:"+x.challenge_no.inspect+", level:"+x.level.inspect+", test_assertions:"+x.test_assertions.inspect+", test_url:"+x.test_url.inspect+", video_explanation_url:"+x.video_explanation_url.inspect+", is_challenge:"+x.is_challenge.inspect+")"
+			puts "Challenge.create(id: "+x.id.inspect+", challenge:"+x.challenge.inspect+", challenge_no:"+x.challenge_no.inspect+", level:"+x.level.inspect+", test_assertions:"+x.test_assertions.inspect+", test_url:"+x.test_url.inspect+", video_explanation_url:"+x.video_explanation_url.inspect+", is_challenge:"+x.is_challenge.inspect+")"
 		end
 	end
 
--- db/migrate/20190108193838_add_did_click_test_page_to_visitor_activities.rb
+++
@@ -1,5 +0,0 @@
-class AddDidClickTestPageToVisitorActivities < ActiveRecord::Migration[5.1]
-  def change
-    add_column :visitor_activities, :did_click_test_page, :boolean
-  end
-end
