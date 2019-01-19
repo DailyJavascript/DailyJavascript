41ea125e4d7f8c7aa004c754ee0090f9005a7b36
unknown <salr3467@gmail.com>
2019-01-19 17:43:45 -0500

    updating associations

--- app/models/visitor_activity.rb
+++ app/models/visitor_activity.rb
@@ -1,6 +1,5 @@
 class VisitorActivity < ApplicationRecord
 	belongs_to :visit
-	belongs_to :user, through: :visit
+	has_one :user, through: :visit
 	#has_one :unsubscribed, through: :visit, primary_key: "user_id", foreign_key: "user_id"
-	#has_one :user, through: :visit
 end
