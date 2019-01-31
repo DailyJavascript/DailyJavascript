8eddaa73194957da4405f5c32db5862bbc8e1f52
srahimi1 <watid2005@aim.com>
2019-01-31 11:37:01 -0500

    changed userchallenges.find to userchallenge.where in mail_next_challenge method in challenge model because .find was throwing RecordNotFound error and was causing mailings to stop at error, was expecting empty record instead of error, so switched to .where which returns empty record instead of error

--- app/models/get_seeds.rb
+++ app/models/get_seeds.rb
@@ -1,13 +1,31 @@
 class GetSeeds < ApplicationRecord
 
 
-	def self.get_refcodes
+	def self.get_refcode_seeds
 		RefCode.order(:id).each do |x|
 			puts "RefCode.create(id: "+x.id.inspect+", ref_code:"+x.ref_code.inspect+", ref_code_url:"+x.ref_code_url.inspect+", recipient_name:"+x.recipient_name.inspect+", recipient_url:"+x.recipient_url.inspect+")"
 		end
 	end
 
-	def self.get_challenges
+	def self.get_challenge_seeds
+		Challenge.order(:id).each do |x|
+			puts "Challenge.create(id: "+x.id.inspect+", challenge:"+x.challenge.inspect+", challenge_no:"+x.challenge_no.inspect+", level:"+x.level.inspect+", test_assertions:"+x.test_assertions.inspect+", test_url:"+x.test_url.inspect+", video_explanation_url:"+x.video_explanation_url.inspect+", is_challenge:"+x.is_challenge.inspect+")"
+		end
+	end
+
+	def self.get_user_seeds
+		User.order(:id).each do |x|
+			puts "User.create(id: "+x.id.inspect+", email:"+x.email.inspect+", email_verified:"+x.email_verified.inspect+", email_verification_code:"+x.email_verification_code.inspect+", unsubscribe_code:"+x.unsubscribe_code.inspect+", date_joined:"+x.date_joined.inspect+", membership_level:"+x.membership_level.inspect+", date_current_membership_level:"+x.date_current_membership_level.inspect+", membership_level_history:"+x.membership_level_history.inspect+", active:"+x.active.inspect+", price_plan_id:"+x.price_plan_id.inspect+")"
+		end
+	end
+
+	def self.get_userchallenge_seeds
+		Challenge.order(:id).each do |x|
+			puts "Challenge.create(id: "+x.id.inspect+", ch:"+x.challenge.inspect+", challenge_no:"+x.challenge_no.inspect+", level:"+x.level.inspect+", test_assertions:"+x.test_assertions.inspect+", test_url:"+x.test_url.inspect+", video_explanation_url:"+x.video_explanation_url.inspect+", is_challenge:"+x.is_challenge.inspect+")"
+		end
+	end
+
+	def self.get_unsubscribed_seeds
 		Challenge.order(:id).each do |x|
 			puts "Challenge.create(id: "+x.id.inspect+", challenge:"+x.challenge.inspect+", challenge_no:"+x.challenge_no.inspect+", level:"+x.level.inspect+", test_assertions:"+x.test_assertions.inspect+", test_url:"+x.test_url.inspect+", video_explanation_url:"+x.video_explanation_url.inspect+", is_challenge:"+x.is_challenge.inspect+")"
 		end
