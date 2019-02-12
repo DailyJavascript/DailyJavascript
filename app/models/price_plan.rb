5516b1674e66c684e03d3e5bb85f2e8e59ece120
srahimi1 <watid2005@aim.com>
2019-02-11 23:50:41 -0500

    test

--- app/models/price_plan.rb
+++ app/models/price_plan.rb
@@ -2,16 +2,16 @@ class PricePlan < ApplicationRecord
 
 
 def self.mail_special_offer_emails
-	[8,175].each do |x|
+	[8,175,160].each do |x|
 		UserMailer.special_offer_email(x, "$4_special_offer").deliver_now
 	end
-	[8,175].each do |x|
+	[8,175,160].each do |x|
 		UserMailer.special_offer_email(x, "$3_special_offer").deliver_now
 	end
-	[8,175].each do |x|
+	[8,175,160].each do |x|
 		UserMailer.special_offer_email(x, "$2_special_offer").deliver_now
 	end
-	[8,175].each do |x|
+	[8,175,160].each do |x|
 		UserMailer.special_offer_email(x, "$1_special_offer").deliver_now
 	end
 end
